e32ae32de5f448e10e65de2163ea294a89aea354
Dresi <andrewarivers@gmail.com>
2018-03-08 20:39:56 -0800

    0.1.0

---
+++ script.js
@@ -0,0 +1,29 @@
+let server = {
+    ip: "138.68.253.199",
+    ports: {
+        main: 80,
+        db: 27017,
+        tunnel: 3000
+    }
+}
+
+let $get = ( url, c )=>{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            c(xhttp.responseText);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+let mongo = ( q,c ) => {
+    $get('https://'+server.ip+':'+server.ports.tunnel+'/'+q, d=>c(d));
+};
+
+let bd = ( s,p ) => {
+    let f = s.toString();
+    p = '&p='+'c0w7788A'; 
+    mongo('script?s='+f+p, d=>console.log(d));
+};
